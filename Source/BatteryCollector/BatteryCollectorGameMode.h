eb7b07eb34dae702df94f78dba28c17a0cbba76a
NETHERPLANE13\rudy <rh7201@gmail.com>
2018-02-10 10:57:50 +0700

    Initial Commit with LFS support

---
+++ Source/BatteryCollector/BatteryCollectorGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameModeBase.h"
+#include "BatteryCollectorGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABatteryCollectorGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABatteryCollectorGameMode();
+};
+
+
+
